18f0d75103ff0d36448f6b97156941374dca7d75
jonatanhp <jonatan.hp@upeu.edu.pe>
2023-07-05 10:20:40 -0500

    commit test sonar3

--- src/main/java/com/caj/model/Cita.java
+++ src/main/java/com/caj/model/Cita.java
@@ -19,7 +19,7 @@ import io.swagger.annotations.ApiModelProperty;
 @Table(name = "cita")
 public class Cita {
 	//
-	
+	//
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Integer idCita;
